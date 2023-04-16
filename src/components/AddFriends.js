11f31c50946db1135f0873d97667bff8a4f2e13c
Terrence Chungong <112122220+terrencechungong@users.noreply.github.com>
2023-04-16 16:49:53 +0000

    started adding add friends logic

--- src/components/AddFriends.js
+++ src/components/AddFriends.js
@@ -3,29 +3,42 @@ import { Home } from "./Home";
 import { useAuth } from '../components/contexts/AuthContexts';
 import axios from "axios";
 
+
+// put chat name in a map and chat people in a list as an attribute
+//test in a list first
+//map next
+
+
 export function AddFriends () {
     const { user } = useAuth();
-     function getData(){
-        let chats;
-         axios.get('https://api.chatengine.io/chats/', {
+    let allPeople = {};
+
+     async function getData(){
+         await axios.get('https://api.chatengine.io/chats/', {
                 headers:{
-                    "project-id": "f9e47d2b-eca9-4ba4-afc5-41dd9a901f93",
+                    "project-id": "271fe4e8-950e-44b3-92cb-24d0ee33a5a2",
                     "user-name": user.email,
                     "user-secret":user.uid
                 }
             }).then((response) => {
-                chats = response.data[0]
+                for(var chats of response.data){
+                    // differentiate between chats and check if chat name is a duplicate
+                    allPeople[chats.title] = [];
+                    for(var personData of chats.people){
+                        allPeople[chats.title].push(`${personData.person.first_name} ${personData.person.last_name} (${personData.person.username})`)
+                    }
+                }
+                console.log(allPeople)
+                //map chat title names to people names
             })
             .catch((error)=>console.log(error.message))
-            return chats
     }
-    let data =  getData()
-    console.log(data)
+
     return (
         <div>
             <Home/>
-            <h1>Add Friends</h1>
-            <p>{data}</p>
+            <button onClick={getData}>Add Friends</button>
+            <ul id="add-here"></ul>
         </div>
     );
 }
