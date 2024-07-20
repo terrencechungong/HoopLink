4a2bb60e5159fff191061b68ec69e1adcb72b20b
unknown <terrencechungong@gmail.com>
2024-07-20 10:52:38 -0700

    files

--- src/components/ChatSettingComponents/ChatFiles.js
+++ src/components/ChatSettingComponents/ChatFiles.js
@@ -1,8 +1,24 @@
+import pic from './piccy.png'
+import defaultpic from './defaultprofile.png'
+import { FaPlus } from 'react-icons/fa';
 
 
 const ChatFiles = () => {
+    const names = ["Liam.jpg", "Emma", "Noah", "Olivia", "William", "Ava", "James", "Isabella", "Oliver", "Sophia"];
+
     return (
-        <p>Chat files </p>
+        <div className="chat-files-modal">
+
+            {names.map(name =>
+                <div className="chat-file-row">
+                    <img src={pic} />
+                    <div>
+                        <p>{name}</p>
+                        <i>Shared by {name} on yesterday</i>
+                    </div>
+                </div>
+            )}
+        </div>
     )
 }
 
