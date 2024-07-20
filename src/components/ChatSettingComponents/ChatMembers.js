aba571e3c72b50e9205c5caf35ea05eb7c9c1a8b
unknown <terrencechungong@gmail.com>
2024-07-19 23:57:23 -0700

    members list code start

--- src/components/ChatSettingComponents/ChatMembers.js
+++ src/components/ChatSettingComponents/ChatMembers.js
@@ -1,8 +1,23 @@
-
+import './styles/chat-settings-members.scss'
+import pic from './piccy.png'
+import defaultpic from './defaultprofile.png'
 
 const ChatMembers = () => {
+    const names = ["Liam", "Emma", "Noah", "Olivia", "William", "Ava", "James", "Isabella", "Oliver", "Sophia"];
+
     return (
-        <p>Chat members</p>
+        <div className="chat-members-modal">
+            <div className="chat-member-row">
+                    <img src={defaultpic} />
+                    <p>+ add a member</p>
+                </div>
+            {names.map(name =>
+                 <div className="chat-member-row">
+                    <img src={pic} />
+                    <p>{name}</p>
+                </div>
+                )}
+        </div>
     )
 }
 
