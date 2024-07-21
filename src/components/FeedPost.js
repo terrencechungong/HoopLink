06e4facc4a90a3ea6ef01bf26ec1bd414dd32896
unknown <terrencechungong@gmail.com>
2024-07-21 13:40:28 -0700

    read more on post + flexi. height

--- src/components/FeedPost.js
+++ src/components/FeedPost.js
@@ -1,7 +1,29 @@
 import './styles/post.scss'
 import pic from './ChatSettingComponents/piccy.png'
+import { useState } from 'react'
+import { FaRegHeart } from "react-icons/fa";
+import { FaRegComment } from "react-icons/fa6";
+import { IoIosSend } from "react-icons/io";
+import { LuSend } from "react-icons/lu";
+import { FaHeart } from "react-icons/fa";
 
 const FeedPost = () => {
+    const [liked, setLiked] = useState(false);
+    const [seeMore, setSeeMore] = useState(false);
+    const text = "Elon Musk is hauling ass on his “Gigafactory of Compute \
+    project in Memphis. But a whiplash deal, NDAs, and backroom \
+    promises made to the city have lawmakers demanding answers. \
+    Musk is hauling ass on his “Gigafactory of Compute \
+    project in Memphis. But a whiplash deal, NDAs, and backroom \
+    promises made to the city have lawmakers demanding answers. \
+    on Musk is hauling ass on his “Gigafactory of Compute \
+    project in Memphis. But a whiplash deal, NDAs, and backroom \
+    promises made to the city have lawmakers demanding answers. \
+    Musk is hauling ass on his “Gigafactory of Compute \
+    project in Memphis. But a whiplash deal, NDAs, and backroom \
+    promises made to the city have lawmakers demanding answers."
+
+
     return (
         <div className='just-2-build'>
             <div className="feed-post-container">
@@ -14,17 +36,40 @@ const FeedPost = () => {
                 </div>
                 <div className="feed-post-content">
                     {/* make what shows up conditional */}
-                    <p>Elon Musk is “hauling ass” on his “Gigafactory of Compute” 
-                        project in Memphis. But a whiplash deal, NDAs, and backroom 
-                        promises made to the city have lawmakers demanding answers.</p>
+                    <DisplayText text={text} />
+                    {/* Add Files */}
+
                 </div>
                 <div className='feed-post-likes-and-comments'>
-                    <button>C</button>
-                    <button>L</button>
-                    <button>S</button>
+                    <button className={liked && 'liked'} onClick={() => setLiked(!liked)}>
+                        {!liked && <FaRegHeart size={22} />}
+                        {liked && <FaHeart size={22} />}
+                    </button>
+                    <button><FaRegComment size={25} /></button>
+                    <button ><LuSend size={22} /></button>
                 </div>
             </div>
         </div>
+    )
+}
+
+
+const DisplayText = ({ text }) => {
+    const [seeMore, setSeeMore] = useState(false);
+    let displayText;
+    if (text.length > 475) {
+        displayText = <p>
+            {text.substring(0, 474)}...<strong id="read-more-post" onClick={() => setSeeMore(true)}>Read More</strong>
+            </p>
+    } else {
+        displayText = <p>{text}</p>
+    }
+
+    return (
+        <>
+            {seeMore && <p>{text}</p>}
+            {!seeMore && displayText}
+        </>
     )
 }
 
