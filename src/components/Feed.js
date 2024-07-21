c1527e4b33b017292853ca1369d011779bcf1007
unknown <terrencechungong@gmail.com>
2024-07-21 14:04:14 -0700

    last before run

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,15 +3,25 @@ import FeedPost from './FeedPost';
 
 const Feed = () => {
     let feedPosts = []
-    for (let i = 0; i<= 40; i++) {
-        feedPosts.push(<FeedPost/>);
+    for (let i = 0; i <= 40; i++) {
+        feedPosts.push(<FeedPost />);
     }
 
     return (
-        <div className="feed"> 
-        {feedPosts}
+        <div className="feed">
+            <CreateAPost />
+            {feedPosts}
         </div>
     );
+}
+
+const CreateAPost = () => {
+    return (
+        <div className='create-a-post'>
+                Create a post my boy
+        </div>
+        // this takes to a new screen not a modal. then they come back to feed after post
+    )
 }
 
 export default Feed;
