60e8b407a6671d73af36febdcc29a23140eeef58
unknown <terrencechungong@gmail.com>
2024-08-04 14:34:09 -0700

    profile view screen

--- src/components/ProfileViewPost.js
+++ src/components/ProfileViewPost.js
@@ -1,10 +1,22 @@
-
+import { useNavigate } from "react-router-dom";
+import { BsPostcard } from "react-icons/bs";
+import './styles/profile-view-post.scss'
 
 const ProfileViewPost = () => {
-    return( 
-    <div>
-        pROFILE VIEW POST BITCJHH
-    </div>
+    const navigate = useNavigate();
+
+    return (
+        <div className='single-post-run-view'>
+            <div className='single-post-run-view-wrap'>
+                <p style={{ color: "white" }}> Profile run bitch</p>
+                <span>&bull;</span>
+                <p className='single-post-run-view-loca' style={{ color: "white" }}>LOCATION</p>
+                <span>&bull;</span>
+                <p className='single-post-run-view-time' style={{ color:  "white" }}>Ended 1w ago</p>
+            </div>
+            <BsPostcard size={30} onClick={() => { navigate('/single-post-view') }} style={{ cursor: 'pointer' }} />
+        </div>
+        //render background different based on start end
     )
 }
 
--- src/components/ProfileViewRun.js
+++ src/components/ProfileViewRun.js
@@ -1,14 +1,20 @@
 import './styles/profile-view-run.scss'
+import { CgInfo } from "react-icons/cg";
+import { useNavigate } from 'react-router-dom';
 
 const ProfileViewRun = () => {
+    const navigate = useNavigate();
+
     return (
         <div className='single-profile-run-view'>
-           <p> Profile run bitch</p>
-           <span>&bull;</span>
-
-           <p className='single-profile-run-view-loca' style={{color:'rgb(110, 110, 110)'}}>LOCATION</p>
-           <span>&bull;</span>
-           <p className='single-profile-run-view-time' style={{color:'rgb(110, 110, 110)'}}>Ended 1w ago</p>
+            <div className='single-profile-run-view-wrap'>
+                <p> Profile run bitch</p>
+                <span>&bull;</span>
+                <p className='single-profile-run-view-loca' style={{ color: 'rgb(110, 110, 110)' }}>LOCATION</p>
+                <span>&bull;</span>
+                <p className='single-profile-run-view-time' style={{ color: 'rgb(110, 110, 110)' }}>Ended 1w ago</p>
+            </div>
+            <CgInfo size={30} onClick={() => { navigate('/single-run-view') }} style={{ cursor: 'pointer' }} />
         </div>
         //render background different based on start end
     )
