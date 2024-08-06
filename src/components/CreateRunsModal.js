e5e1e909fa9c0e9186d4f6486785164f20e50874
unknown <terrencechungong@gmail.com>
2024-08-06 09:24:44 -0700

    bout2work

--- src/components/CreateRunsModal.js
+++ src/components/CreateRunsModal.js
@@ -3,21 +3,44 @@ import { IoCloseOutline } from "react-icons/io5";
 import { useRef, useState } from 'react';
 import { TimeComponent, StartDateCalendar } from './RunStartDateAndTime';
 import { AnimatePresence } from 'framer-motion';
+import pic from './ChatSettingComponents/piccy.png'
 
 const CreateRunsModal = ({ closeModalFunction }) => {
     const startDateInputRef = useRef(null);
     const [showCalendar, setShowCalendar] = useState(false);
     const [showStartTime, setShowStartTime] = useState(false);
-
+    const [friendInviteFocused, setFriendInvitesFocused] = useState(false)
     const startDateValSet = useRef(null);
-    const startTimeValSet = useRef(null)
+    const startTimeValSet = useRef(null);
+    const personSeletorRef = useRef(null);
 
     const startDateStyle = {
-        backgroundColor:'rgb(201, 223, 255)',
+        backgroundColor: 'rgb(201, 223, 255)',
         padding: '4px',
         cursor: 'pointer',
         borderRadius: '7px'
     }
+    // fix
+    let peopleOptions = [];
+    for (let i = 0; i <= 4; i++) {
+        peopleOptions.push(
+            <div data-key={`${i}`} className='individual-dropdown-selection'
+                onClick={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation()
+                    personSeletorRef.current.focus();
+                    console.log(Number(e.currentTarget.getAttribute('data-key')));
+                    peopleOptions.splice(Number(e.currentTarget.getAttribute('data-key')), 1)
+                }}
+            >
+                <img src={pic} />
+                <p>
+                   {`${i} yoyo`}
+                </p>
+            </div>)
+    }
+
+    // keep div focused
 
 
     return (
@@ -31,7 +54,7 @@ const CreateRunsModal = ({ closeModalFunction }) => {
                 {showCalendar && <StartDateCalendar inputRef={startDateValSet} handleClose={() => setShowCalendar(false)} />}
             </AnimatePresence>
 
-            <div id="create-a-run-modal">
+            <div id="create-a-run-modal" onClick={() => setFriendInvitesFocused(false)}>
                 <div id="create-a-run-header">
                     <h2><strong>Create A Run</strong></h2>
                     <button onClick={closeModalFunction}><IoCloseOutline size={32} /></button>
@@ -47,25 +70,36 @@ const CreateRunsModal = ({ closeModalFunction }) => {
                     </div>
                     <div className='create-run-content-section' >
                         <label>Start Date</label>
-                        <div 
+                        <div
                             ref={startDateValSet}
                             onClick={() => setShowCalendar(true)}
                             style={startDateStyle} >Start date</div>
                     </div>
                     <div className='create-run-content-section'>
                         <label>Start Time</label>
-                        <div 
+                        <div
                             ref={startTimeValSet}
                             onClick={() => setShowStartTime(true)}
                             style={startDateStyle} >Start time</div>
                     </div>
+                    {/* add end time */}
                     <div className='create-run-content-section'>
                         <label>Location</label>
                         <input type="text" placeholder='Court Address...' />
                     </div>
-                    <div className='create-run-content-section'>
+                    <div className='create-run-content-section' style={{ position: 'relative' }}>
                         <label>Choose Friends to Invite</label>
-                        <input type="text" placeholder='gONNA BE LIKE THE META PERSON SELECTOR' />
+                        <div className="editable-friends-to-invite-div" contentEditable={true}
+                            tabIndex={0} ref={personSeletorRef} type="text" placeholder='gONNA BE LIKE THE META PERSON SELECTOR' onClick={(e) => {
+                                e.stopPropagation()
+                                setFriendInvitesFocused(true)}}></div>
+                        {friendInviteFocused && <div className='selection-options-dropdown' onClick={(e) => {
+                            e.preventDefault();
+                            e.stopPropagation()
+                            personSeletorRef.current.focus();
+                        }}>
+                            {peopleOptions}
+                        </div>}
                     </div>
                     <br></br>
                     <div className='create-run-content-section'>
