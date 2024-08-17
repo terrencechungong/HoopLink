ad26ba68cb192e345c348ea98f21f39de28badaf
unknown <terrencechungong@gmail.com>
2024-08-17 12:08:59 -0700

    add and remove from user section

--- src/components/NewChatModal.js
+++ src/components/NewChatModal.js
@@ -13,7 +13,13 @@ const NewChatModal = ({ closeModal }) => {
     const [showFriendsList, setShowFriendsList] = useState(false);
     const index = useRef(0);
     const [addedUsers, setAddedUsers] = useState([]);
-    const [userOptions, setUserOptions] = useState([]);
+    const [userOptions, setUserOptions] = useState([{ profilePhoto: pic, name: "eiroerieoir", id: uuidv4() },
+    { profilePhoto: pic, name: "eiroerieoir", id: uuidv4() },
+    { profilePhoto: pic, name: "eiroerieoir", id: uuidv4() },
+    { profilePhoto: pic, name: "eiroerieoir", id: uuidv4() },
+    { profilePhoto: pic, name: "eiroerieoir", id: uuidv4() },
+    { profilePhoto: pic, name: "eiroerieoir", id: uuidv4() }
+    ]);
 
     useEffect(() => {
         const func = function (event) {
@@ -50,41 +56,9 @@ const NewChatModal = ({ closeModal }) => {
             return text
         }
     }
-    let strings = ['sdjfnaskdfjnsjdnfksadf', "yoo", "terrence", "chungong", "sds"]
-    let peopleOptions = [];
-    let pplOps2 = [];
-    for (let i = 0; i < 1; i++) {
-        peopleOptions.push(
-            <div data-key={`${i}`} className='individual-dropdown-selection'
-                onClick={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation()
-                    console.log(Number(e.currentTarget.getAttribute('data-key')));
-                }}
-            >
-                <img src={pic} />
-                <p>
-                    {`${concatNameForDropDown(strings[i])}`}
-                </p>
-            </div>)
+   
 
-        pplOps2.push(
-            <div id={`person${i}`} className='individual-dropdown-selection'
-                onClick={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation()
-                    console.log(Number(e.currentTarget.getAttribute('data-key')));
-                }}
-            >
-                <img src={pic} />
-                <p>
-                    {`${concatNameForDropDown(strings[i])}`}
-                </p>
-                <IoCloseCircleOutline size={15} />
-            </div>)
-    }
-
-    const addUserToSection = () => {
+    const addUserToSection = (userId) => {
         if (index.current == 1) {
             const userSection = document.getElementById(`added-members-to-section`);
             userSection.style.width = '246px';
@@ -93,11 +67,14 @@ const NewChatModal = ({ closeModal }) => {
             const userSection = document.getElementById(`added-members-to-section`);
             userSection.style.width = '366px';
         }
-        setAddedUsers((prev) => [...prev, { profilePhoto: pic, name: "eiroerieoir", id: uuidv4() }]);
-        index.current += 1
+        const user = userOptions.find(user => user.id === userId);
+        setAddedUsers((prev) => [...prev, user]);
+        index.current += 1;
+        // removeUserFromOptions(userId)
     }
 
     const removeFromUserSection = (userId) => {
+        const user = addedUsers.find(user => user.id === userId);
         setAddedUsers(addedUsers.filter(user => user.id !== userId));
         if (index.current == 3) {
             const userSection = document.getElementById(`added-members-to-section`);
@@ -107,19 +84,9 @@ const NewChatModal = ({ closeModal }) => {
             const userSection = document.getElementById(`added-members-to-section`);
             userSection.style.width = '126px';
         }
-        index.current -= 1
+        index.current -= 1;
+        // addUserToOptions(user)
     }
-
-
-    const addUserToOptions = () => {
-
-    }
-
-    const removeUserFromOptions = () => {
-
-    }
-
-
 
 
     // on hover show more info asbout the user
@@ -137,28 +104,42 @@ const NewChatModal = ({ closeModal }) => {
                     <h2><strong>Add members to your new chat: </strong></h2>
                     <div id='added-members-to-section'>
                         {addedUsers.map((user, index) => (
-                            <div id={`added-person-${index}`} className='individual-dropdown-selection'
+                            <div key={user.id} id={`added-person-${index}`} className='individual-dropdown-selection'
                                 onClick={(e) => {
                                     e.preventDefault();
                                     e.stopPropagation()
+                                    // removeUserFromOptions(user.id)
                                 }}
                             >
                                 <img src={user.profilePhoto} />
                                 <p>
                                     {`${concatNameForDropDown(user.name)}`}
                                 </p>
-                                <IoCloseCircleOutline size={15} onClick={() => {removeFromUserSection(user.id)}}/>
+                                <IoCloseCircleOutline size={15} onClick={() => { removeFromUserSection(user.id) }} />
                             </div>
                         ))}
                     </div>
                 </div>
-                <div style={{position:'relative'}}>
-                {true && <div className='selection-options-added-members' onClick={(e) => {
+                <div style={{ position: 'relative' }}>
+                    <div className='selection-options-added-members' onClick={(e) => {
                         e.preventDefault();
                         e.stopPropagation()
                     }}>
-                        {peopleOptions} {peopleOptions}
-                    </div>}
+                        {userOptions.filter(userOption => !addedUsers.some(addedUser => addedUser.id === userOption.id))
+                            .map((user, index) =>
+                                <div key={user.id} id={`user-${index}`} className='individual-dropdown-selection'
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        e.stopPropagation();
+                                        addUserToSection(user.id);
+                                    }}
+                                >
+                                    <img src={user.profilePhoto} />
+                                    <p>
+                                        {`${concatNameForDropDown(user.name)}`}
+                                    </p>
+                                </div>)}
+                    </div>
                     <h2><strong>Description: </strong></h2>
                     <textarea placeholder='Ex: Chat for the greatest hoopers in the city'></textarea>
                 </div>
